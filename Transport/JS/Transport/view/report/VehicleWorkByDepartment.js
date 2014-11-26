ed1eec79467b991c172047f3cd77b586989c34f3
Dklenye <dmtKlenye@gmail.com>
2014-11-26 13:38:39 +0300

    Добавлен отчёт о работе транспорта по подразделениям

---
+++ Transport/JS/Transport/view/report/VehicleWorkByDepartment.js
@@ -0,0 +1,41 @@
+﻿
+T.view.report.VehicleWorkByDepartment = Ext.extend(Kdn.view.Report, {
+
+    reportName: 'VehicleWorkByDepartment',
+
+    params: [
+       '-',
+            'Период:',
+            {
+               xtype:'datefield',
+               dataIndex:'start',
+               value:new Date(),
+               width:120
+            },
+            '-', 
+            {
+               xtype:'datefield',
+               dataIndex:'end',
+               value:new Date(),
+               width:120
+           },
+            '-',
+            {
+                xtype: 'combo.transportcolumn',
+                dataIndex:'columnId',
+                objectValue:false,
+                enableClear:true
+            }
+   ],
+
+    buildReportParams: function(params) {
+        var p = {};
+        p.start = params.start;
+        p.end = params.end;
+        p.columnId = params.columnId||0;
+        return p;
+    }
+
+});
+
+Ext.reg('view.report.vehicleworkbydepartment', T.view.report.VehicleWorkByDepartment);
